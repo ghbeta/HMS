96cce7099dcc7566f2d2a71f0f7df8e52275b463
Hao <ghmail2@gmail.com>
2015-05-28 15:27:38 +0200

    first version of git repo

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,7 +1,25 @@
 package controllers;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.Session;
 import models.User;
+import nl.minicom.gitolite.manager.exceptions.GitException;
+import nl.minicom.gitolite.manager.exceptions.ModificationException;
+import nl.minicom.gitolite.manager.exceptions.ServiceUnavailable;
+import nl.minicom.gitolite.manager.models.Config;
+import nl.minicom.gitolite.manager.models.ConfigManager;
+import nl.minicom.gitolite.manager.models.Group;
+import nl.minicom.gitolite.manager.models.Permission;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
 import org.eclipse.jgit.internal.storage.file.FileRepository;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.transport.JschConfigSessionFactory;
+import org.eclipse.jgit.transport.OpenSshConfig;
+import org.eclipse.jgit.transport.SshSessionFactory;
+import org.eclipse.jgit.util.FS;
 import play.*;
 import play.api.libs.Crypto;
 import play.data.DynamicForm;
@@ -14,12 +32,14 @@ import views.html.*;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.InetAddress;
 
 import static play.libs.Json.toJson;
 public class Application extends Controller {
     public static User currentuser = null;
     public static Result index() {
         return ok(index.render("HMS"));
+        //return ok(register.render());
     }
 
     public static Result addUser(){
@@ -55,5 +75,64 @@ public class Application extends Controller {
         }
 
         return ok("leave it on purpose");
+    }
+
+    public static Result addRemoteRepo() throws ModificationException, ServiceUnavailable, IOException, GitException, InterruptedException, GitAPIException, JSchException {
+        //String host_current_user=System.getProperty("user.name");
+        //System.out.println("llllllllllllllllllllllllllll"+host_current_user);
+
+        DynamicForm requestData = Form.form().bindFromRequest();
+        String host=requestData.get("hostname");
+        String pubkey=requestData.get("pubkey");
+        String repo = requestData.get("repo");
+        Repository adminrepo = new FileRepository("/home/hao/gitolite-admin/.git");
+        Git gitogit = new Git(adminrepo);
+        SshSessionFactory.setInstance(new JschConfigSessionFactory() {
+            @Override
+            protected void configure(OpenSshConfig.Host host, Session session) {
+                session.setConfig("StrictHostKeyChecking","no");
+            }
+        });
+        ConfigManager manager = ConfigManager.create("/home/hao/gitolite-admin");
+        //ConfigManager manager = ConfigManager.create("git@localhost:gitolite-admin");
+        Config config = manager.get();
+
+            nl.minicom.gitolite.manager.models.User repouser = config.createUser(currentuser.matrikel);
+
+            config.createRepository(repo).setPermission(repouser, Permission.ALL);
+            repouser.setKey(host,pubkey);
+
+        manager.apply(config);
+        gitogit.pull().call();
+        //gitogit.commit().setMessage("Add User").call();
+        gitogit.push().call();
+//        JSch jsch = new JSch();
+//        jsch.addIdentity("id_rsa");
+//        Session session = jsch.getSession("git","localhost",22);
+//        java.util.Properties prop = new java.util.Properties();
+//        prop.put("StrictHostKeyChecking","no");
+//        session.setConfig(prop);
+//        session.connect();
+//        SshSessionFactory factory = new JschConfigSessionFactory() {
+//            @Override
+//            protected void configure(OpenSshConfig.Host host, Session session) {
+//                session.setConfig("StrictHostKeyChecking","no");
+//            }
+//            protected JSch getJSch(final OpenSshConfig.Host hc, FS fs) throws  JSchException{
+//                JSch jsch = super.getJSch(hc,fs);
+//                jsch.removeAllIdentity();
+//                jsch.addIdentity("id_rsa");
+//                return jsch;
+//            }
+//        };
+
+        //gitogit.push().call();
+
+
+
+
+
+
+        return ok("git address: git@"+ InetAddress.getLocalHost()+":"+repo+".git");
     }
 }
