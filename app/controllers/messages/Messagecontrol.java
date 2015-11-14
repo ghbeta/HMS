e47d12289d5e8a5f5fe12f3ea13ed6e260c7be62
Hao <ghmail2@gmail.com>
2015-11-14 23:04:49 +0100

    do not need real time chat

--- app/controllers/messages/Messagecontrol.java
+++ app/controllers/messages/Messagecontrol.java
@@ -10,25 +10,5 @@ import play.mvc.WebSocket;
  */
 public class Messagecontrol extends Controller {
 
-      public static WebSocket<String> messagesockets(){
 
-          return WebSocket.whenReady((in, out) -> {
-              // For each event received on the socket,
-
-              in.onMessage(new F.Callback<String>() {
-                  @Override
-                  public void invoke(String s) throws Throwable {
-                      out.write("aaa");
-
-
-                  }
-              });
-
-              // When the socket is closed.
-              in.onClose(() -> System.out.println("Disconnected"));
-
-              // Send a single 'Hello!' message
-              out.write("Hello!");
-          });
-      }
 }
