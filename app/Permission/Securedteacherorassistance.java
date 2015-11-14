dd3cf966402abffccf071b58f4fedd23a1740501
Hao <ghmail2@gmail.com>
2015-11-14 18:08:47 +0100

    add secured check assitant or teacher

---
+++ app/Permission/Securedteacherorassistance.java
@@ -0,0 +1,46 @@
+package Permission;
+
+import models.User;
+import models.UserRoll;
+import play.Logger;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Security;
+import views.html.forbidden;
+
+/**
+ * Created by Hao on 2015/11/14.
+ */
+public class Securedteacherorassistance extends Security.Authenticator{
+
+    @Override
+    public String getUsername(Http.Context ctx) {
+        Logger.info(ctx.session().get("email"));
+        User current=User.findByEmail(ctx.session().get("email"), "global");
+        //Logger.warn(current.roles);
+        if(current!=null) {
+            if (current.roles == null) {
+                current.roles = "";
+            }
+            if (current.roles.equals(UserRoll.Teachers.toString())||current.roles.equals(UserRoll.Assistants.toString())) {
+                Logger.warn("authorized user is allowed");
+                return ctx.session().get("email");
+            } else {
+                return null;
+            }
+        }else{
+            return null;
+        }
+
+        //        return ctx.session().get("roles");
+
+    }
+
+    @Override
+    public Result onUnauthorized(Http.Context ctx) {
+        Logger.info(ctx.session().get("email"));
+        User current=User.findByEmail(ctx.session().get("email"), "global");
+        return ok(forbidden.render(current));
+
+    }
+}
