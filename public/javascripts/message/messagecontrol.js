1412ee5faa1be9080b781991d7bc3d05994ca257
Hao <ghmail2@gmail.com>
2015-11-16 02:08:56 +0100

    rewrite the function in angular js, this time all works

--- public/javascripts/message/messagecontrol.js
+++ public/javascripts/message/messagecontrol.js
@@ -5,29 +5,29 @@
 var convjson={};
 var semester1="";
 var username={};
-function filter(semester){
-    var posturl="/messages/"+semester;
-    semester1=semester;
-    var xhr= new XMLHttpRequest();
-    xhr.open('POST',posturl);
-    xhr.setRequestHeader('Content-Type', 'text/plain');
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4 && xhr.status == 200) {
-            //alert(xhr.responseText);
-            //convjson=angular.fromJson(xhr.responseText);
-            setconvjson(xhr.responseText);
-
-            //threads=JSON.parse(xhr.responseText);
-            //console.log(threads)
-            //forum.scope.$apply()
-            //$route.reload();
-        }
-    }
-
-    xhr.send();
-
-
-}
+//function filter(semester){
+//    var posturl="/messages/"+semester;
+//    semester1=semester;
+//    var xhr= new XMLHttpRequest();
+//    xhr.open('POST',posturl);
+//    xhr.setRequestHeader('Content-Type', 'text/plain');
+//    xhr.onreadystatechange = function () {
+//        if (xhr.readyState == 4 && xhr.status == 200) {
+//            //alert(xhr.responseText);
+//            //convjson=angular.fromJson(xhr.responseText);
+//            setconvjson(xhr.responseText);
+//
+//            //threads=JSON.parse(xhr.responseText);
+//            //console.log(threads)
+//            //forum.scope.$apply()
+//            //$route.reload();
+//        }
+//    }
+//
+//    xhr.send();
+//
+//
+//}
 
 function setconvjson(json){
     convjson.init=JSON.parse(json);
@@ -36,49 +36,79 @@ function setconvjson(json){
 
 }
 
-function participant(selfemail,user1email,user2email){
-    var resultemail="";
-    var name="";
-    if(selfemail===user1email){
-        resultemail=user2email;
-        //return user2email;
-    }
-    if(selfemail===user2email){
-        resultemail=user1email;
-        //return user1email
-    }
-    var posturl="/messages/"+semester1+"/"+resultemail;
-    console.log(posturl);
-
-    //var xhr= new XMLHttpRequest();
-    //xhr.open('POST',posturl);
-    //xhr.setRequestHeader('Content-Type', 'text/plain');
-    //xhr.onreadystatechange = function () {
-    //    if (xhr.readyState == 4 && xhr.status == 200) {
-    //        //alert(xhr.responseText);
-    //        //convjson=angular.fromJson(xhr.responseText);
-    //        var userobject= JSON.parse(xhr.responseText);
-    //        name= userobject.firstname+" "+userobject.lastname;
-    //        //convjson.scope.$apply();
-    //        //threads=JSON.parse(xhr.responseText);
-    //        //console.log(xhr.responseText)
-    //        //forum.scope.$apply()
-    //        //$route.reload();
-    //    }
-    //}
-
-    xhr.send();
-    console.log(name);
-    return name;
-}
+//function participant(selfemail,user1email,user2email){
+//    var resultemail="";
+//    var name="";
+//    if(selfemail===user1email){
+//        resultemail=user2email;
+//        //return user2email;
+//    }
+//    if(selfemail===user2email){
+//        resultemail=user1email;
+//        //return user1email
+//    }
+//    var posturl="/messages/"+semester1+"/"+resultemail;
+//    console.log(posturl);
+//
+//    //var xhr= new XMLHttpRequest();
+//    //xhr.open('POST',posturl);
+//    //xhr.setRequestHeader('Content-Type', 'text/plain');
+//    //xhr.onreadystatechange = function () {
+//    //    if (xhr.readyState == 4 && xhr.status == 200) {
+//    //        //alert(xhr.responseText);
+//    //        //convjson=angular.fromJson(xhr.responseText);
+//    //        var userobject= JSON.parse(xhr.responseText);
+//    //        name= userobject.firstname+" "+userobject.lastname;
+//    //        //convjson.scope.$apply();
+//    //        //threads=JSON.parse(xhr.responseText);
+//    //        //console.log(xhr.responseText)
+//    //        //forum.scope.$apply()
+//    //        //$route.reload();
+//    //    }
+//    //}
+//
+//    //xhr.send();
+//    console.log(name);
+//    return name;
+//}
 
 var app = angular.module('messages',[]);
 app.controller('messagecontroller',function($scope,$http){
-    
-    $scope.filter=filter;
+    $scope.filter=function filter(semester){
+        semester1=semester;
+        var posturl="/messages/"+semester;
+        $http({method: 'GET',
+               url:posturl,
+               headers:{'Content-Type':'text/plain'}}).success(function(response){
+
+            $scope.talks=response;
+            console.log(response);
+        })
+    };
     $scope.username=username.fullname;
-    $scope.talks=convjson;
-    convjson.scope=$scope;
-    console.log(convjson);
-    $scope.participant=participant;
+    //$scope.talks=convjson;
+    //convjson.scope=$scope;
+    //console.log(convjson);
+    $scope.participant=function participant(selfemail,user1email,user2email){
+        var resultemail="";
+        var name="";
+        if(selfemail===user1email){
+            resultemail=user2email;
+            //return user2email;
+        }
+        if(selfemail===user2email){
+            resultemail=user1email;
+            //return user1email
+        }
+        //console.log("WS2016");
+        var posturl="/messages/"+semester1+"/"+resultemail;
+        console.log(posturl);
+        $http({method: 'GET',
+            url:posturl,
+            headers:{'Content-Type':'text/plain'}}).success(function(response){
+            $scope.otheruser=response;
+            //return $scope.otheruser.lastname;
+            console.log(response);
+        })
+    };
 })
