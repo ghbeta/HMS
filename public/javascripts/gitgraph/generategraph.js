6a6263247a5cee1e71fa4fd20f58747d1fefb069
Hao <ghmail2@gmail.com>
2015-11-20 19:29:52 +0100

    modify the student homepage to show the canvas

---
+++ public/javascripts/gitgraph/generategraph.js
@@ -0,0 +1,27 @@
+/**
+ * Created by Hao on 2015/11/20.
+ */
+function showcanvas(useremail,semester,lecture,user){
+    var posturl="/"+semester+"/"+lecture+"/"+useremail+"/repostatus#home";
+    console.log(posturl);
+    var xhr= new XMLHttpRequest();
+    xhr.open('GET',posturl);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            //alert(xhr.responseText);
+            var gitgraph = new GitGraph({
+                elementId:user,
+                template: "metro",
+                orientation: "horizontal",
+                mode: "compact"
+            });
+            var master=gitgraph.branch("master");
+            var commithistory=JSON.parse(xhr.responseText);
+            for(i=0;i<commithistory.length();i++){
+                  master.commit();
+            }
+        }
+    };
+    xhr.send();
+
+}
