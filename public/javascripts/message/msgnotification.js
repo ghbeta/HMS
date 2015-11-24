3d493afb75869b2ec7daef1480596609629dcf7b
Hao <ghmail2@gmail.com>
2015-11-24 14:06:22 +0100

    add system wide notification(delete the unneeded part at angularjs). one page two angular app is not really good supported

---
+++ public/javascripts/message/msgnotification.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Hao on 2015/11/24.
+ */
+
+$(document).ready(
+  connectws()
+);
+
+function connectws(){
+    var wsuri = "ws://"+location.host+"/websocket";
+    var websocket;
+    websocket= new WebSocket(wsuri);
+    websocket.onopen=function(evt){onOpen(evt)};
+    websocket.onmessage=function(evt){onMessage(evt)};
+}
+
+function onOpen(evt){
+    console.log("connected");
+}
+
+function onMessage(evt){
+    console.log(evt.data);
+    var msg=JSON.parse(evt.data);
+    if(msg.event==="noti"){
+        var namepart=msg.data.split(";")[0];
+        alert("new messages from" + namepart);
+    }
+}
