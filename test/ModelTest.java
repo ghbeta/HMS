9574bcc50c2d91a0f53a3b5fc63005463eaaf476
Hao <ghmail2@gmail.com>
2016-01-04 18:10:33 +0100

    add more model test

--- test/ModelTest.java
+++ test/ModelTest.java
@@ -55,8 +55,37 @@ public class ModelTest {
         Helpers.stop(model);
     }
 
+    public Semesteruser newSemesteruser(){
+        Semesteruser semesteruser=new Semesteruser();
+        semesteruser.id="7788414";
+        semesteruser.save("WS2016");
+        return semesteruser;
+    }
+    public Semesteruser newSemesteruser(String id){
+        Semesteruser semesteruser=new Semesteruser();
+        semesteruser.id=id;
+        semesteruser.save("WS2016");
+        return semesteruser;
+    }
+
+    public Lecture newLecture(){
+        Lecture lecture = new Lecture();
+        lecture.courseName="test";
+        lecture.save("WS2016");
+        return lecture;
+    }
+
+    public Message newMessage(){
+        Message message=new Message();
+        message.messagebody="abc";
+        message.semester="WS2016";
+        message.setTimestamp();
+        message.save("WS2016");
+        return message;
+    }
+
    @Test
-   public void a_testUser() throws AppException {
+   public void testUser() throws AppException {
        User student = new User();
        student.id="7788414";
        student.email="a@a.com";
@@ -70,4 +99,67 @@ public class ModelTest {
        student.save("global");
        assertThat(User.findById("7788414","global")).isNotNull();
    }
+    @Test
+    public void testSSH(){
+        User owner= new User();
+        owner.id="7788414";
+        owner.save("global");
+        SSH ssh= new SSH();
+        ssh.ssh="abc";
+        ssh.title="title";
+        ssh.sshowner=owner;
+        ssh.save("global");
+        assertThat(SSH.findById("1")).isNotNull();
+    }
+
+    @Test
+    public void testRepo(){
+        Repo repo=new Repo();
+        repo.setRepofilepath("abc");
+        repo.repopath="abc2";
+        repo.owner.add(newSemesteruser());
+
+        repo.course=newLecture();
+        repo.semester="WS2016";
+        repo.save("WS2016");
+        assertThat(Repo.findRepoByLectureAndOwner("WS2016",repo.owner.get(0),repo.course)).isNull();
+    }
+
+    @Test
+    public void testMessage(){
+        Message message=new Message();
+        message.messagebody="abc";
+        message.semester="WS2016";
+        message.setTimestamp();
+        Conversation conversation= new Conversation();
+        conversation.semester="WS2016";
+        conversation.save("WS2016");
+        message.conversation=conversation;
+
+        message.sender=newSemesteruser();
+        message.save("WS2016");
+        assertThat(Message.findAllByConversation("WS2016",conversation)).hasSize(1);
+    }
+
+    @Test
+    public void testEvaluation(){
+        Evaluation evaluation=new Evaluation();
+       evaluation.lecture=newLecture();
+        evaluation.student=newSemesteruser();
+        evaluation.setPerformance("WS2016",evaluation.lecture,evaluation.student);
+        evaluation.save("WS2016");
+        assertThat(Evaluation.findByLectureAndUser("WS2016",evaluation.lecture,evaluation.student)).isNotNull();
+    }
+
+    @Test
+    public void testConversation(){
+        Conversation conversation=new Conversation();
+        conversation.user1=newSemesteruser();
+        conversation.user2=newSemesteruser("7788520");
+        conversation.semester="WS2016";
+        conversation.messages.add(newMessage());
+        conversation.save("WS2016");
+        assertThat(Conversation.getConversationById("WS2016","1")).isNotNull();
+
+    }
 }
