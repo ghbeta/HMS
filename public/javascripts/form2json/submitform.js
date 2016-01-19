bede6ba88ff957834b077707d0fcb61d1c234c3f
Hao <456@456.com>
2016-01-19 22:33:00 +0100

    fix remember accordion open status

--- public/javascripts/form2json/submitform.js
+++ public/javascripts/form2json/submitform.js
@@ -1,14 +1,15 @@
 /**
  * Created by Hao on 2015/11/5.
  */
-;(function($){
+//;(function($){
 
     /**
      * Store scroll position for and set it after reload
      *
      * @return {boolean} [loacalStorage is available]
      */
-    $.fn.scrollPosReaload = function(){
+    //$.fn.scrollPosReaload = function(){
+    function scrollPosReaload(){
         if (localStorage) {
             var posReader = localStorage["posStorage"];
 
@@ -25,30 +26,51 @@
         }
 
         return false;
-    };
+    }
 
-
-    /* ================================================== */
-
-    $(document).ready(function() {
-        // Feel free to set it for any element who trigger the reload
-
-        var collapseItem = localStorage.getItem('collapseItem');
-        if (collapseItem) {
-            $(collapseItem).collapse('show')
-            }
-        $('select').scrollPosReaload();
-        $('a').click(function() {
-            //store the id of the collapsible element
-            if($(this).attr('href').indexOf("Assignment")){
-            localStorage.setItem('collapseItem', $(this).attr('href'));
-                console.log("click sth"+$(this).attr('href'));}
-
-        });
-
+$(document).ready(function () {
+    $('.markstatus').click(function() {
+        //store the id of the collapsible element
+        localStorage.setItem('collapseItem', $(this).attr('href'));
     });
 
-}(jQuery));
+    var collapseItem = localStorage.getItem('collapseItem');
+    if (collapseItem) {
+        $(collapseItem).collapse('show')
+    }
+});
+    /* ================================================== */
+
+    //$(document).ready(function() {
+    //    // Feel free to set it for any element who trigger the reload
+    //
+    //    var collapseItem = localStorage.getItem('collapseItem');
+    //    if (collapseItem) {
+    //        $(collapseItem).collapse('show')
+    //        }
+    //   // $('select').scrollPosReaload();
+    //    $('a').click(function() {
+    //        //store the id of the collapsible element
+    //        //if($(this).attr('href').indexOf("Assignment")){
+    //        localStorage.setItem('collapseItem', $(this).attr('href'));
+    //            console.log("click sth"+$(this).attr('href'));//}
+    //
+    //    });
+    //    //var last=$.cookie('activeAccordionGroup');
+    //    //if (last!=null) {
+    //    //    //remove default collapse settings
+    //    //    $(".collapse").removeClass('shown');
+    //    //    //show the account_last visible group
+    //    //    $("#" + last).addClass("shown");
+    //    //}
+    //});
+
+//$("#homeworkcorrectionlist").bind('shown', function() {
+//    var active=$(".shown").attr('id');
+//    $.cookie('activeAccordionGroup', active)
+//});
+
+//}(jQuery));
 
 
 //function submit(formid,semester,lecture,assignment,student)
@@ -74,12 +96,27 @@
 //    xhr.send(JSON.stringify(formData));
 //}
 
-function copytoclip(text){
+//function copytoclip(text){
+//
+//    var gitadress = document.querySelector('.copytext');
+//    var range = document.createRange();
+//    range.selectNode(gitadress);
+//    window.getSelection().addRange(range);
+//    document.execCommand("copy");
+//
+//}
 
-    var gitadress = document.querySelector('.copytext');
-    var range = document.createRange();
-    range.selectNode(gitadress);
-    window.getSelection().addRange(range);
-    document.execCommand("copy");
-
-}
+//var c = document.cookie;
+//
+//$('.collapse').each(function () {
+//    if (this.id) {
+//        var pos = c.indexOf(this.id + "_collapse_in=");
+//        if (pos > -1) {
+//            c.substr(pos).split('=')[1].indexOf('false') ? $(this).addClass('in') : $(this).removeClass('in');
+//        }
+//    }
+//}).on('hidden shown', function () {
+//    if (this.id) {
+//        document.cookie = this.id + "_collapse_in=" + $(this).hasClass('in');
+//    }
+//});
