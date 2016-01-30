37d327612568d0314b277479d6f3a9de8311c30d
Hao <456@456.com>
2016-01-30 15:18:55 +0100

    add schedule job to clean up repotowatch database and remove the access right from the user

--- app/Global.java
+++ app/Global.java
@@ -3,6 +3,8 @@ import models.*;
 import org.h2.tools.Server;
 import play.*;
 import play.Application;
+import play.libs.Akka;
+import scala.concurrent.duration.Duration;
 import utils.CreateAdmin;
 import utils.FileWatcher.RepoWatcher;
 
@@ -13,11 +15,16 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+import java.util.concurrent.TimeUnit;
+
 import static java.nio.file.StandardWatchEventKinds.ENTRY_MODIFY;
 import static utils.CreateDB.createServer;
 import static utils.FileWatcher.InitWatchService.getWatchService;
 //import static utils.FileWatcher.InitWatchService.registerALL;
 import static utils.FileWatcher.InitWatchService.setWatchService;
+import static utils.RepoManager.AccessChangerforEvaluation;
 
 /**
  * Created by Hao on 2015/6/15.
@@ -30,8 +37,8 @@ public class Global extends GlobalSettings{
         Logger.info("starting file watch service");
         try {
             setWatchService();
-            RepoWatcher repoWatcher= new RepoWatcher(getWatchService());
-            Thread watchThread=new Thread(repoWatcher,"repoWatcherThread");
+            RepoWatcher repoWatcher = new RepoWatcher(getWatchService());
+            Thread watchThread = new Thread(repoWatcher, "repoWatcherThread");
             watchThread.start();
             //Path toWatch= Paths.get(System.getProperty("user.home"),"repositories","WS2016_LocalLectureTest_7788414.git","logs","refs","heads");
             //Path toWatch= Paths.get(System.getProperty("user.home"),"repositories");
@@ -97,11 +104,11 @@ public class Global extends GlobalSettings{
             //createServer("SS2016", entity1);
             //createServer("WS2016", entity1);
         }
-        List<RepoToWatch> allreponames= RepoToWatch.findAllReponame();
-        if(allreponames.size()>0){
-            for(RepoToWatch repoWatcher:allreponames){
-                Path toWatch= Paths.get(System.getProperty("user.home"),"repositories",repoWatcher.reponame,"refs","heads");
-                Logger.warn(toWatch.toString()+" was added to watch");
+        List<RepoToWatch> allreponames = RepoToWatch.findAllReponame();
+        if (allreponames.size() > 0) {
+            for (RepoToWatch repoWatcher : allreponames) {
+                Path toWatch = Paths.get(System.getProperty("user.home"), "repositories", repoWatcher.reponame, "refs", "heads");
+                Logger.warn(toWatch.toString() + " was added to watch");
                 try {
                     toWatch.register(getWatchService(), ENTRY_MODIFY);
                 } catch (IOException e) {
@@ -110,6 +117,49 @@ public class Global extends GlobalSettings{
                 }
             }
         }
+
+        Akka.system().scheduler().schedule(
+                Duration.create(ExcutionDelay(0, 0), TimeUnit.SECONDS),
+                Duration.create(7, TimeUnit.DAYS),
+                new Runnable(){
+                    @Override
+                    public void run() {
+                        Logger.info("start RepoToWatch database clean Job");
+                        List<RepoToWatch> allrepos=RepoToWatch.findAllReponame();
+                        if(allrepos.size()>0){
+                            for(RepoToWatch repo : allrepos){
+                                String[] informationpart=repo.reponame.split("_");
+                                String lecturename=informationpart[1];
+                                String semester=informationpart[0];
+                                Lecture lecture= Lecture.getlecturebyname(lecturename,semester);
+                                if(lecture!=null&&!lecture.isExpired()){
+                                    String userid=informationpart[2].replace(".git","");
+                                    String reponame =repo.reponame;
+                                    AccessChangerforEvaluation(userid,reponame.replace(".git",""),true);
+                                    repo.delete("global");
+
+                                }
+
+
+                            }
+                        }
+                    }
+                },
+                Akka.system().dispatcher()
+        );
+
+
+
+    }
+
+    public static int ExcutionDelay(int hour, int minute){
+        return Seconds.secondsBetween(
+                new DateTime(),
+                new DateTime().withHourOfDay(hour)
+                .withMinuteOfHour(minute)
+                .withSecondOfMinute(0)
+                .withMillisOfSecond(0)
+        ).getSeconds();
     }
 }
 
